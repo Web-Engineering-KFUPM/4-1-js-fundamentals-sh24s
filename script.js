f0d294f9bf931f698e63f420595dde1334d60514
sh24s <144224701+sh24s@users.noreply.github.com>
2025-09-15 12:36:41 +0300

    Done!

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 2.1 — declare & reassign
 // Create let course = "CIS101"; display it usinng console.log(course), reassign to "CIS102"; display it again.
 // write code here
-
+let course = "CIS102";
+console.log(course);
 
 // Task 2.2 — const safety
 // Create const SCHOOL = "MyCollege"; then TRY to reassign it and observe the Console error. Add a comment explaining    why  reassignment fails.
 // NOTE: For now, keep the reassignment line commented so the file runs without errors.
 // const SCHOOL = "MyCollege";
 // SCHOOL = "OtherCollege"; // <-- Uncomment to see the error, then re-comment after testing
-
+const SCHOOL = "MyCollege";
 
 // ==========================
 // TODO-3: ARITHMETIC & TYPES
@@ -23,13 +24,16 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 3.1 — arithmetic basics
 // Given let x = 8, y = 3; log x+y, x-y, x*y, x/y, x%y.
 // write code here
-
+let x = 8, y = 3;
+console.log (x+y, x-y, x*y, x/y, x%y)
 
 // Task 3.2 — number vs string
 // Display results of "2" + 3, 2 + "3", and 2 + 3. Add a short comment: why do the first two concatenate?
 // write code here
-
-
+// beacuse they are different type
+console.log ("2" + 3)
+console.log ( 2 + "3")
+console.log (2 + 3)
 // Read chapter 4 in zyBooks: Compound Assignment Operators
 
 
@@ -45,6 +49,17 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 //         - "Young" if age is between 13 and 35
 //         - "Aged" if age > 35
 // write code here
+let age = prompt("Enter your age:");
+if (age< 13) {
+    console.log("Child")
+} 
+else if (13 < age < 35) {
+    console.log("Young")
+
+  }
+   else {
+    console.log("Aged")
+  }
 
 
 // Task 4.2 — Switch statement
@@ -54,6 +69,27 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 //         - If it is "Sat" or "Sun", log "weekend".
 //         - For any other value, log "unknown".
 // write code here
+let day = "Mon"
+switch (day) {
+    case "Mon":
+        console.log ("weekday")
+      break;
+    case "Tue":
+        console.log ("weekday")
+    case "Wed":
+        console.log ("weekday")
+    case "Thu":
+        console.log ("weekday")
+    case "Fri":
+        console.log ("weekday")
+    case "Sat":
+        console.log ("weekend")
+    case "Sun":
+        console.log ("weekend")
+      break;
+    default:
+      console.log ("unknown")
+  }
 
 // Read Chapter 4 in zyBooks: Conditional (ternary) operator
 
@@ -64,12 +100,20 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 5.1 — for loop sum
 // TODO: Sum integers 1..10 with a for loop; display the result of total sum.
 // write code here
-
+let i = 1;
+while (i <= 11) {
+   console.log(i);
+   i++;
+}
 
 // Task 5.2 — while loop
 // let t = 3; while(t > 0), decrement the value t variable in each iteration and display the result.
 // write code here
-
+let t = 3;
+while (t>0) {
+   console.log(t);
+   t++;
+}
 
 // Read Chapter 4 in zyBooks: Do-While Loop
 // write code here
@@ -83,11 +127,20 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Make a function add(a,b){ return a+b; } display the result of add(2,5).
 // write code here
 
+function add(a,b){ 
+    return a+b;
+ }
+ console.log(add(2,5))
+
 
 // Task 6.2 — Arrow functions
 // Make an arrow function; const cube = n => n*n*n; cube(3)
 // write code here
-
+const cube = n 
+function arrow (cube){
+    return n = n*n*n
+}
+console.log(cube(3))
 
 // =================================
 // TODO-7: SCOPE & GLOBAL OBJECT (ESSENTIALS)
@@ -96,7 +149,10 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 7.1 — var vs let scope
 // Declare var a = 1; let b = 2 inside a block { },  then try displaying both outside the block; observe differences.
 // write code here
-
+var a = 1
+let b = 2
+console.log(a)
+console.log(b)
 
 // ==================
 // TODO-8: ARRAYS (CORE)
@@ -105,6 +161,11 @@ console.log("%cJS Lab Connected — Start completing the TODOs!", "font-weight:b
 // Task 8.1 — create & mutate
 // TODO: let nums = [3,1,4]; then push(1), unshift(9), pop(); log final array and length.
 // write code here
-
+let nums = [3,1,4];
+nums.push(1)
+nums.unshift(9)
+nums.pop();
+console.log(nums)
+console.log(nums.length)
 
 // End of manual — great job! Keep this file open and work task by task.
